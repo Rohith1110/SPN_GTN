d860d3539f101c3a42874a2298a8739f53d9e99a
vchenn6-uic <144352425+Rohith1110@users.noreply.github.com>
2024-11-07 10:40:50 -0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,23 +5,28 @@ from pathlib import Path
 import torch
 from torch_geometric.datasets import PPI
 from torch_geometric.data import (InMemoryDataset, Data, DataLoader)
-import torch_geometric.transforms as T
+from torch_geometric.transforms import NormalizeFeatures
 
 
 class ProcessedDataset(InMemoryDataset):
-    pass
+    def __init__(self, root, transform=None, pre_transform=None):
+        super().__init__(root, transform, pre_transform)
 
 
 def precompute_edge_label_and_reverse(dataset: InMemoryDataset):
+    """Precomputes edge labels and reverse edge mappings."""
     data_list = []
     for data in dataset:
         u, v = data.edge_index
         yu, yv = data.y[u], data.y[v]
         data.edge_labels = yu * dataset.num_classes + yv
 
-        edge_dict = torch.sparse_coo_tensor(indices=data.edge_index, values=torch.arange(data.num_edges), size=(data.num_nodes, data.num_nodes)).to_dense()
+        edge_dict = torch.sparse_coo_tensor(
+            indices=data.edge_index,
+            values=torch.arange(data.edge_index.size(1)),
+            size=(data.num_nodes, data.num_nodes)
+        ).to_dense()
         data.edge_index_reversed = edge_dict[v, u]
-
         data_list.append(data)
 
     new_data, new_slices = InMemoryDataset.collate(data_list)
@@ -41,47 +46,57 @@ class BinaryPPI(PPI):
 
 
 def prepare_PPI(args: Namespace, path=osp.join('.', 'data', 'PPI')):
+    """Prepares PPI dataset with optional graph reduction for testing small graphs."""
     gid, lid = map(int, args.dataset.split('-')[1:])
     assert gid in range(1, 21), f'gid should be in 1-20, got {gid}'
     assert lid in range(121), f'lid should be in 0-120, got {lid}'
 
     def transform(data):
         data.y = data.y[:, lid].long()
+
+        # Retain small graphs only
+        if data.num_nodes > 5:  # Adjust the threshold as required
+            new_data = Data()
+            new_data.x = data.x[:5]
+            new_data.edge_index = data.edge_index[:, data.edge_index[0] < 5]
+            new_data.edge_index = new_data.edge_index[:, new_data.edge_index[1] < 5]
+            new_data.y = data.y[:5]
+            return new_data
         return data
 
     train_dataset = BinaryPPI(path, split='train', transform=transform)[list(range(gid))]
     val_dataset = BinaryPPI(path, split='val', transform=transform)
     test_dataset = BinaryPPI(path, split='test', transform=transform)
+
     return train_dataset, val_dataset, test_dataset
 
 
 class CitationDataset(InMemoryDataset):
-    def __init__(self, root=None, split='train', transform=None, pre_transform=None, pre_filter=None):
-        assert split in ['train', 'val', 'test']
-        super(CitationDataset, self).__init__(root, transform, pre_transform, pre_filter)
-
+    def __init__(self, root=None, split='train', transform=None, pre_transform=None):
+        super().__init__(root, transform, pre_transform)
         saved_data = torch.load(root)
-        
-        self.data = Data(edge_index=saved_data['{}_e'.format(split)], x=saved_data['{}_x'.format(split)], y=saved_data['{}_y'.format(split)])
+        self.data = Data(
+            edge_index=saved_data[f'{split}_e'],
+            x=saved_data[f'{split}_x'],
+            y=saved_data[f'{split}_y']
+        )
         num_nodes = self.data.x.size(0)
         num_edges = self.data.edge_index.size(1)
         self.slices = {
-            'x': torch.LongTensor([0, num_nodes]), 
+            'x': torch.LongTensor([0, num_nodes]),
             'y': torch.LongTensor([0, num_nodes]),
             'edge_index': torch.LongTensor([0, num_edges])
         }
 
 
 class BatchedCitationDataset(InMemoryDataset):
-    def __init__(self, root=None, transform=None, pre_transform=None, pre_filter=None):
-        super(BatchedCitationDataset, self).__init__(root, transform, pre_transform, pre_filter)
-
+    def __init__(self, root=None, transform=None, pre_transform=None):
+        super().__init__(root, transform, pre_transform)
         self.data = torch.load(root)
-        
         num_nodes = self.data.x.size(0)
         num_edges = self.data.edge_index.size(1)
         self.slices = {
-            'x': torch.LongTensor([0, num_nodes]), 
+            'x': torch.LongTensor([0, num_nodes]),
             'y': torch.LongTensor([0, num_nodes]),
             'edge_index': torch.LongTensor([0, num_edges]),
             'batch': torch.LongTensor([0, num_edges])
@@ -89,32 +104,31 @@ class BatchedCitationDataset(InMemoryDataset):
 
 
 def prepare_dblp(args: Namespace):
+    """Prepares the DBLP dataset."""
     path = osp.join('.', 'data', 'Citation', 'dblp.pkl')
 
-    train_dataset = CitationDataset(root=path, split='train')  #, transform=T.NormalizeFeatures())
-    val_dataset = CitationDataset(root=path, split='val')  #, transform=T.NormalizeFeatures())
-    test_dataset = CitationDataset(root=path, split='test')  #, transform=T.NormalizeFeatures())
+    train_dataset = CitationDataset(root=path, split='train')
+    val_dataset = CitationDataset(root=path, split='val')
+    test_dataset = CitationDataset(root=path, split='test')
+
     return train_dataset, val_dataset, test_dataset
 
 
 def prepare_dataloaders(args: Namespace):
     """
-    Prepare train, valid and test dataloaders.
-    Args:
-        args: Arguments from the command line. See config.py.
-
-    Returns:
-        train_loader: DataLoader for training set.
-        val_loader: DataLoader for validation set.
-        test_loader: DataLoader for test set.
+    Prepare train, valid, and test dataloaders for a given dataset.
     """
     if args.dataset.startswith('ppi-'):
-        train_dataset, val_dataset, test_dataset = map(precompute_edge_label_and_reverse, prepare_PPI(args))
+        train_dataset, val_dataset, test_dataset = map(
+            precompute_edge_label_and_reverse, prepare_PPI(args)
+        )
         train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
         val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
         test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
     elif args.dataset == 'dblp':
-        train_dataset, val_dataset, test_dataset = map(precompute_edge_label_and_reverse, prepare_dblp(args))
+        train_dataset, val_dataset, test_dataset = map(
+            precompute_edge_label_and_reverse, prepare_dblp(args)
+        )
         train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
         val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
         test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
@@ -123,11 +137,17 @@ def prepare_dataloaders(args: Namespace):
         train_loader = BatchedCitationDataset(root=root / f'{args.dataset}_train.pt')
         val_loader = BatchedCitationDataset(root=root / f'{args.dataset}_val.pt')
         test_loader = BatchedCitationDataset(root=root / f'{args.dataset}_test.pt')
-        train_loader, val_loader, test_loader = map(precompute_edge_label_and_reverse, (train_loader, val_loader, test_loader))
+        train_loader, val_loader, test_loader = map(
+            precompute_edge_label_and_reverse, (train_loader, val_loader, test_loader)
+        )
+
         def _set_dataset_attr(loader):
             loader.dataset = loader
             return loader
-        train_loader, val_loader, test_loader = map(_set_dataset_attr, (train_loader, val_loader, test_loader))
+
+        train_loader, val_loader, test_loader = map(
+            _set_dataset_attr, (train_loader, val_loader, test_loader)
+        )
     else:
         raise NotImplementedError(f'Dataset {args.dataset} not supported.')
     return train_loader, val_loader, test_loader
